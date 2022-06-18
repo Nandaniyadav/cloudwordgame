c2941af8a2a9a9976617bdbe3f6d0318ed91f240
Nandani Yadav <103631451+nandaniy454@users.noreply.github.com>
2022-06-18 20:28:03 +0530

    .

---
+++ PythonWordsetup.py
@@ -0,0 +1,39 @@
+import cx_Freeze
+import sys
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+shortcut_table = [
+    ("DesktopShortcut",  # Shortcut
+     "DesktopFolder",  # Directory_
+     "Blast Cloud Word Game",  # Name
+     "TARGETDIR",  # Component_
+     "[TARGETDIR]\PygameWordgame.exe",  # Target
+     None,  # Arguments
+     None,  # Description
+     None,  # Hotkey
+     None,  # Icon
+     None,  # IconIndex
+     None,  # ShowCmd
+     "TARGETDIR",  # WkDir
+     )
+]
+msi_data = {"Shortcut": shortcut_table}
+
+# Change some default MSI options and specify the use of the above defined tables
+bdist_msi_options = {'data': msi_data}
+
+executables = [cx_Freeze.Executable(script="PygameWordgame.py",icon='cc.ico',base=base)]
+
+cx_Freeze.setup(
+    version="1.0",
+    description="Pygame BLast Cloud Game",
+    author="Nandani Yadav",
+    name="Blast Cloud Word Game",
+    options={"build_exe": {"packages":["pygame"],
+                           "include_files":['cc.ico','jj.png','1.png','2.png','clipart984165.png','explosion1.gif','bgmuisc.wav','boom.wav','loss1.wav','Blast Cloud Word Game-1.0-amd64 (1).msi','cc.pnp']},
+             "bdist_msi": bdist_msi_options,
+             },
+    executables = executables
+
+    )
---
+++ pygamewordgame.py
@@ -0,0 +1,106 @@
+import cx_Freeze
+def new_word():
+    global word_x,word_y,text,chosen_word,press_word,speed
+    word_x = random.randint(100,600)
+    word_y = 0
+    chosen_word = random.choice(llist)
+    press_word = ''
+    speed +=0.02
+    text = font.render(chosen_word,True,black)
+import pygame
+import sys
+import random
+import time
+pygame.init()
+############################## colors
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+black = (0,0,0)
+########################## variables
+x = 900
+y= 600
+speed = 0.1
+point = 0
+press_word = ''
+llist = ['apple','ball','cat','dog','egg','fish','girl','horse','ice','jokor','kite','lion','monkey','nest','orange','parrot','queen','rat','sun','tree','umbrella','van','watch','xray','yak','zebra']
+font = pygame.font.SysFont('ComicSansMs',45)
+text1 = font.render('Hit Enter to restart the Game', True,red)
+##################### screen
+win = pygame.display.set_mode((x,y))
+pygame.display.set_caption('Blast Cloud Word Game')
+
+################## images
+logo = pygame.image.load('jj.png')
+background1 =  pygame.image.load('1.png')
+background2 =  pygame.image.load('2.png')
+cloud =  pygame.image.load('clipart984165.png')
+matchimg = pygame.image.load('explosion1.gif')
+pygame.display.set_icon(logo)
+
+########################## sounds
+pygame.mixer.music.load('bgmuisc.wav')
+channel0 = pygame.mixer.Channel(0)
+channel0.set_volume(0.3)
+channel01= pygame.mixer.Channel(1)
+channel01.set_volume(0.3)
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.play()
+blastmusic = pygame.mixer.Sound('boom.wav')
+lostmusic = pygame.mixer.Sound('loss1 (1).wav')
+
+################################
+new_word()
+#################################################### infinite loop
+while True:
+    if(point >= 20):
+        win.blit(background2,(0,0))
+    else:
+         win.blit(background1,(0,0))
+    win.blit(cloud, (word_x-40, word_y-20))
+
+    win.blit(text,(word_x,word_y))
+    word_y += speed
+    word_caption = font.render(chosen_word,True,red)
+    win.blit(word_caption,(10,50))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif(event.type == pygame.KEYDOWN):
+            press_word += pygame.key.name(event.key)
+            if chosen_word.startswith(press_word):
+                text = font.render(press_word,True,blue)
+                if(chosen_word == press_word):
+                    point += len(chosen_word)
+                    channel0.play(blastmusic,maxtime=600)
+                    channel0.set_volume(0.3)
+                    win.blit(matchimg,(word_x,word_y))
+                    pygame.display.update()
+                    time.sleep(0.02)
+                    new_word()
+            else:
+                text = font.render(press_word,True,red)
+                press_word = ''
+
+    point_caption = font.render(str(point),True,blue)
+    win.blit(point_caption,(10,5))
+    if(word_y < y-5):
+        pass
+    else:
+        bb = pygame.mixer.Channel(1).get_busy()
+        if bb == 1:
+            pass
+        else:
+            channel01.play(lostmusic,maxtime=6000)
+        win.blit(text1,(100,260))
+        pygame.display.update()
+        event = pygame.event.wait()
+        if(event.type == pygame.QUIT):
+            pygame.quit()
+            sys.exit()
+        if(event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+            point = 0
+            speed = 1
+            new_word()
+    pygame.display.update()
